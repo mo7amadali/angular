681129d193f878ccd7c62db08e2d65edc8244ce7
Mohamed Ali <mohamedali@aucegypt.edu>
2015-12-12 23:56:52 +0200

    Module 2 Finished

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -1,8 +1,11 @@
 'use strict';
 eventsApp.controller("EditEventController",function($scope){
-$scope.saveEvent=function(event){
-
+$scope.saveEvent=function(event,newEventForm){
+if (newEventForm.$valid) {
+	console.log(newEventForm);
 	alert(event.name);
+};
+	
 };
 $scope.cancelEdit=function(){
 	window.location="/eventDetails.html";
